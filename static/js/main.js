3c048355b5384e3dcff548ee6b905b3e67a9a5f0
Taeeun99 <gfhjkuji@naver.com>
2022-11-27 05:12:50 +0900

    메인페이지 게시물  한페이지에 6개씩 테이블을 사용해서 정렬 완료

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,21 @@
+// 필터 스크롤
+function $(el) {
+  return  document.querySelector(el);
+}
 
+function slid() {
+// input value
+var sv=$("#slid").value;
+// filter contenar width minus perent width divide input max range value multi input value
+var pw=(($("#filters").clientWidth-$("#contenar").clientWidth)/50)*sv;
+$("#filters").style="right:"+pw+"px";
+
+// the width of the cerlc
+var cer=$("#cerlc").offsetWidth;
+// cerlc perent width minus the cerlc width divide input max range multi input value
+var iw=(($("#slid").clientWidth-cer)/50)*sv;
+$("#cerlc").style="left:"+iw+"px";
+}
 
 // // 슬라이드 내 이미지 관련=======================================================
 const galleryItem = document.querySelectorAll(".gallery-item");
